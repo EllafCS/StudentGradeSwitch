2deca60aff0b3a3482eca38859dc6e40b1d7d8be
EllafCS <elafal.jaramez2018@gmail.com>
2024-08-05 19:11:44 +0300

    Initial Commit

---
+++ StudentGradeBySwitch.playground/Contents.swift
@@ -0,0 +1,16 @@
+var grade = 59
+
+switch grade {
+    
+case 90...100 :
+    print (" Exellent")
+case 80...89 :
+    print ("Very Good")
+case 70...79 :
+    print ("Good")
+case 60...69 :
+    print ("Pass")
+default:
+    print ("Fail")
+
+}
